1a36d87e9d83d21c32b2d92c0eb26d4718cbb973
Joint Solutions Technology <josstecnoo@gmail.com>
2024-04-27 14:46:19 +0700

    Fix get order analitic and remove checking mobile number

--- src/helper/censorPhoneNumber.ts
+++ src/helper/censorPhoneNumber.ts
@@ -1,10 +1,5 @@
 export const censorPhoneNumber = (phoneNumber: string) => {
     phoneNumber = phoneNumber.toString();
-
-    if (phoneNumber.length !== 12) {
-        return "Nomor ponsel tidak valid";
-    }
-
     const prefix = phoneNumber.substring(0, 3);
     const suffix = phoneNumber.substring(9);
 
