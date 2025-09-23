85af620fbc3c00542ea9a75daab1ccf5725f7359
irham <i.maulana.azn@gmail.com>
2025-09-23 11:56:18 +0700

    fix: set SameSite attribute for session cookie

--- src/api/admin/loginAdmin.ts
+++ src/api/admin/loginAdmin.ts
@@ -98,6 +98,7 @@ const main: RequestHandler = async (req, res) => {
     httpOnly: true,
     maxAge: config.maxAgeLogin * 1000,
     // domain: config.domainReseller,
+    sameSite:"none",
     path: process.env.NODE_ENV.toLowerCase() === "production" ? "/" : "/v1",
     secure: process.env.NODE_ENV.toLowerCase() === "production",
   });
